7813b0217f12628d097c2177a71c9cb9cfe64c7a
NaturalSpanking <cahutap7@inbox.ru>
2021-11-14 01:05:38 +0500

    Отвязан HAL
    Переделаны функции опроса.
    Добавлена структура инициализации.

--- encoder_ctrl.c
+++ encoder_ctrl.c
@@ -2,14 +2,15 @@
  * encoder_ctrl.c
  *
  *  Created on: 22 θών. 2020 γ.
- *      Author: User
+ *      Author: Maksim Cherkasov
  */
 
 #include "encoder_ctrl.h"
-#include "main.h"
-#include "tim.h"
 
 EncResult queue_array[10];
+EncInit_t* enc_cfg;
+uint32_t enc_timer;
+
 int queue_counter = 0;
 
 void queue_put(EncResult data) {
@@ -19,7 +20,7 @@ void queue_put(EncResult data) {
 	}
 }
 
-EncResult queue_get() {
+EncResult CheckEncoder() {
 	EncResult res = ENC_NOTHING;
 	if (queue_counter) {
 		res = queue_array[0];
@@ -31,7 +32,12 @@ EncResult queue_get() {
 	return res;
 }
 
-EncResult CheckEncoder() { // every 5 ms tick
+void EncInit(EncInit_t* init_struct) {
+	enc_cfg = init_struct;
+	enc_timer = 0;
+}
+
+void EncTick() { // every 5 ms tick
 	static int btn_pressed_counter = 0;
 	static int btn_free_counter = 0;
 	static int click_counter;
@@ -39,72 +45,68 @@ EncResult CheckEncoder() { // every 5 ms tick
 	static uint32_t speed_counter;
 	static uint16_t last_counter;
 	EncResult result = ENC_NOTHING; //nothing
-/******************************* BUTTON ****************************/
-	if (!HAL_GPIO_ReadPin(ENC_BTN_GPIO_Port, ENC_BTN_Pin)) {
-		btn_pressed_counter++;
-		btn_free_counter = 0;
-	} else {
-		btn_pressed_counter = 0;
-		btn_free_counter++;
-	}
-	if (btn_pressed_counter == 2) { //10ms
-		click_counter++;
-	}
-	if ((btn_free_counter == 25) && (click_counter)) { //95ms
-		result = click_counter > 1 ? ENC_DOUBLE : ENC_SHORT;
-		click_counter = 0;
-	}
-	if (btn_pressed_counter == 100) { // 500ms long click
-		btn_pressed_counter = 101;
-		click_counter = 0;
-		result = ENC_LONG;
-	}
-/*************************** ENCODER ******************************/
-	speed_tmr++;
-	uint16_t c_tmp = __HAL_TIM_GET_COUNTER(&htim1);
-	if(abs(c_tmp-last_counter)>=2){
-		if(c_tmp>last_counter){
-			result = btn_pressed_counter ? ENC_RIGHT_PRS:(speed_tmr-speed_counter<=7 ? ENC_RIGHT_FAST:ENC_RIGHT);
-		}else{
-			result = btn_pressed_counter ? ENC_LEFT_PRS:(speed_tmr-speed_counter<=7 ? ENC_LEFT_FAST:ENC_LEFT);
+
+	uint32_t tmp = enc_cfg->GetSysTick();
+	if (tmp - enc_timer >= 5) {
+		enc_timer = tmp;
+		/******************************* BUTTON ****************************/
+		if (!enc_cfg->GetBtnState()) {
+			btn_pressed_counter++;
+			btn_free_counter = 0;
+		} else {
+			btn_pressed_counter = 0;
+			btn_free_counter++;
 		}
-		btn_pressed_counter=101;
-		click_counter=0;
-		last_counter=c_tmp;
-		speed_counter=speed_tmr;
+		if (btn_pressed_counter == 2) { //10ms
+			click_counter++;
+		}
+		if ((btn_free_counter == 25) && (click_counter)) { //95ms
+			result = click_counter > 1 ? ENC_DOUBLE : ENC_SHORT;
+			click_counter = 0;
+		}
+		if (btn_pressed_counter == 100) { // 500ms long click
+			btn_pressed_counter = 101;
+			click_counter = 0;
+			result = ENC_LONG;
+		}
+		/*************************** ENCODER ******************************/
+		speed_tmr++;
+		uint16_t c_tmp = enc_cfg->GetTimerCounter();
+		if (abs(c_tmp - last_counter) >= 2) {
+			if (c_tmp > last_counter) {
+				result =
+						btn_pressed_counter ?
+								ENC_RIGHT_PRS :
+								(speed_tmr - speed_counter <= 7 ?
+										ENC_RIGHT_FAST : ENC_RIGHT);
+			} else {
+				result =
+						btn_pressed_counter ?
+								ENC_LEFT_PRS :
+								(speed_tmr - speed_counter <= 7 ?
+										ENC_LEFT_FAST : ENC_LEFT);
+			}
+			btn_pressed_counter = 101;
+			click_counter = 0;
+			last_counter = c_tmp;
+			speed_counter = speed_tmr;
+		}
+		queue_put(result);
 	}
-	return result;
 }
 
-//void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim) {
-//	HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-//	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3) {
-//		queue_put(ENC_RIGHT);
-//
-//	}
-//	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_4) {
-//		queue_put(ENC_LEFT);
-//
-//	}
-//	__HAL_TIM_SET_COUNTER(htim, 0);
+
+//void Enc_init(){
+//	encoder_init_struct.GetSysTick = HAL_GetTick;
+//	encoder_init_struct.GetBtnState = enc_read_btn;
+//	encoder_init_struct.GetTimerCounter = enc_get_counter;
+//	EncInit(&encoder_init_struct);
 //}
-
-//void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-//	int16_t tmp = __HAL_TIM_GET_COUNTER(htim);
-//	HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-//	if (tmp == 2) {
-//		enc_queue_put(ENC_RIGHT);
-//		__HAL_TIM_SET_COUNTER(htim, 0);
-//	} else if (tmp == -2) {
-//		enc_queue_put(ENC_LEFT);
-//		__HAL_TIM_SET_COUNTER(htim, 0);
-//	} else if (tmp > 2
-//			|| tmp < -2) {
-//		__HAL_TIM_SET_COUNTER(htim, 0);
-//	}
-
-//}
-
-//void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 //
+//uint16_t enc_get_counter(){
+//	return  __HAL_TIM_GET_COUNTER(&htim1);
+//}
+//
+//uint8_t enc_read_btn(){
+//	return HAL_GPIO_ReadPin(ENC_BTN_GPIO_Port,ENC_BTN_Pin);
 //}
--- encoder_ctrl.h
+++ encoder_ctrl.h
@@ -2,8 +2,10 @@
  * encoder_ctrl.h
  *
  *  Created on: 22 θών. 2020 γ.
- *      Author: User
+ *      Author: Maksim Cherkasov
  */
+
+#include "stdint.h"
 
 #ifndef INC_ENCODER_CTRL_H_
 #define INC_ENCODER_CTRL_H_
@@ -12,8 +14,14 @@ typedef enum {
 	ENC_NOTHING, ENC_SHORT, ENC_DOUBLE, ENC_LONG, ENC_LEFT, ENC_LEFT_FAST, ENC_LEFT_PRS, ENC_RIGHT, ENC_RIGHT_FAST, ENC_RIGHT_PRS
 } EncResult;
 
+typedef struct{
+	uint32_t (*GetSysTick)();
+	uint16_t (*GetTimerCounter)();
+	uint8_t (*GetBtnState)();
+}EncInit_t;
 
-
+void EncInit(EncInit_t* init_struct);
+void EncTick();
 EncResult CheckEncoder();
 
 #endif /* INC_ENCODER_CTRL_H_ */
